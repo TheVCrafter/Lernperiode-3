7982e468c46cf5e6a066498eb541be39ae568f5f
TheVCrafter <vincent.devries@bluewin.ch>
2025-01-10 11:31:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Namespace
+{
+    internal class Programm
+    {
+        static void Main(string[] Args)
+        {
+            var p = new Person() { FirstName = "Vincent", LastName = "De Vries", Birthday = new DateTime(2007, 11, 23) };
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public string LastName;
+        public string FirstName;
+        public DateTime Birthday;
+    }
+}
